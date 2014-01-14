989a7110ecd7f8bdcd8bfdbc99afdc6033c8254d
Lars Kunze <kula78@gmail.com>
2014-01-14 09:35:35 +0000

    added echo service to test baseline performances

---
+++ strands_qsr_echo_classifier/src/echo_classification_service.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('strands_qsr_echo_classifier')
+
+from strands_qsr_msgs.srv import *
+from strands_qsr_msgs.msg import *
+import rospy
+import random
+
+def handle_group_classification(req):
+
+    res = GetGroupClassificationResponse()
+
+    res.group_classification = req.group_classification
+    
+    # for obj in req.object_id:
+        
+    #     obj_classification = ObjectClassification()
+
+    #     obj_classification.object_id = obj
+    #     obj_classification.type  = list()
+    #     obj_classification.confidence  = list()
+
+    #     for t in req.type:
+    #         obj_classification.type.append(t)
+    #         obj_classification.confidence.append(random.uniform(0,1))
+
+    #     res.group_classification.append(obj_classification)
+
+    return res
+
+def echo_classification_server():
+    rospy.init_node('echo_classification_server')
+    s = rospy.Service('group_classification', GetGroupClassification, handle_group_classification)
+    rospy.loginfo("Ready to classify groups")
+    rospy.spin()
+
+if __name__ == "__main__":
+    echo_classification_server()
