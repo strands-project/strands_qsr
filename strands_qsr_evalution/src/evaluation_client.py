11c0fad4c61951d25c7e2d75538fd36b2dc23349
cburbridge <cburbridge@gmail.com>
2014-01-15 16:15:42 +0000

    Adding a perception model to the evaluation client.

--- strands_qsr_evalution/src/evaluation_client.py
+++ strands_qsr_evalution/src/evaluation_client.py
@@ -10,6 +10,8 @@ from strands_qsr_msgs.srv import *
 
 from geometry_msgs.msg import *
 
+import perception
+
 OBJ_TYPES = ['Bottle',
              'Book',
              'Calculator',
@@ -39,7 +41,7 @@ def classification_client(request):
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
 
-def gen_object_classification(obj,types,init_method):
+def gen_object_classification(obj,types,perception_distribution):
 
     # Fake object perception
     # - purely random
@@ -47,103 +49,19 @@ def gen_object_classification(obj,types,init_method):
     # - ground truth
 
     obj_classification = ObjectClassification()
-
-    if init_method == 'ground_truth':
-        obj_classification.object_id = obj
-        obj_classification.type  = list()
-        obj_classification.confidence  = list()
-            
-        for t in OBJ_TYPES:
-            obj_classification.type.append(t)
-            if types[obj] == t:
-                obj_classification.confidence.append(1.0)
-            else:
-                obj_classification.confidence.append(0.0)
-        
-    #elif init_method == 'occurrence':
-    #    pass
-    elif init_method == 'ground_truth_60':
-        obj_classification.object_id = obj
-        obj_classification.type  = list()
-        obj_classification.confidence  = list()
-
-        obj_type = types[obj]
-        if random.random() > 0.6: # 40% failure
-            while obj_type == types[obj]:
-                obj_type = random.choice(OBJ_TYPES)
-        
-        for t in OBJ_TYPES:
-            obj_classification.type.append(t)
-            if obj_type == t:
-                obj_classification.confidence.append(1.0)
-            else:
-                obj_classification.confidence.append(0.0)
-
-    elif init_method == 'ground_truth_70':
-        obj_classification.object_id = obj
-        obj_classification.type  = list()
-        obj_classification.confidence  = list()
-
-        obj_type = types[obj]
-        if random.random() > 0.7: # 30% failure
-            while obj_type == types[obj]:
-                obj_type = random.choice(OBJ_TYPES)
-        
-        for t in OBJ_TYPES:
-            obj_classification.type.append(t)
-            if obj_type == t:
-                obj_classification.confidence.append(1.0)
-            else:
-                obj_classification.confidence.append(0.0)
-
-    elif init_method == 'ground_truth_80':
-        obj_classification.object_id = obj
-        obj_classification.type  = list()
-        obj_classification.confidence  = list()
-
-        obj_type = types[obj]
-        if random.random() > 0.8: # 20% failure
-            while obj_type == types[obj]:
-                obj_type = random.choice(OBJ_TYPES)
-        
-        for t in OBJ_TYPES:
-            obj_classification.type.append(t)
-            if obj_type == t:
-                obj_classification.confidence.append(1.0)
-            else:
-                obj_classification.confidence.append(0.0)
-
-    elif init_method == 'ground_truth_90':
-        obj_classification.object_id = obj
-        obj_classification.type  = list()
-        obj_classification.confidence  = list()
-
-        obj_type = types[obj]
-        if random.random() > 0.9: # 10% failure
-            while obj_type == types[obj]:
-                obj_type = random.choice(OBJ_TYPES)
-        
-        for t in OBJ_TYPES:
-            obj_classification.type.append(t)
-            if obj_type == t:
-                obj_classification.confidence.append(1.0)
-            else:
-                obj_classification.confidence.append(0.0)
-
-                
-    else: # init_method == 'random'
-        obj_classification.object_id = obj
-        obj_classification.type  = list()
-        obj_classification.confidence  = list()
-            
-        for t in OBJ_TYPES:
-            obj_classification.type.append(t)
-            obj_classification.confidence.append(random.uniform(0,1))
+    
+    obj_classification.type  = list()
+    obj_classification.confidence  = list()
+    obj_classification.object_id = obj
+    
+    conf =  perception_distribution.create_perception({obj: types[obj],})
+    obj_classification.type = perception_distribution.object_types
+    obj_classification.confidence = conf[obj]
 
     return obj_classification
 
 
-def evaluate_scene(no,scn,init_method):
+def evaluate_scene(no,scn,perception_distribution):
 
     req = GetGroupClassificationRequest()
 
@@ -192,7 +110,7 @@ def evaluate_scene(no,scn,init_method):
             
         req.bbox.append(bbox)
 
-        req.group_classification.append(gen_object_classification(obj,scn['type'],init_method))
+        req.group_classification.append(gen_object_classification(obj,scn['type'],perception_distribution))
              
 
     res = classification_client(req)
@@ -212,7 +130,8 @@ def evaluate_scene(no,scn,init_method):
 
         #print res.group_classification[i]
         #print max_idx
-        print cls, '(', gt, ')'
+        print cls, '(', gt, ')', 
+        print "confidence=", res.group_classification[i].confidence[max_idx]
             
         if gt not in req.type:
             other += 1
@@ -240,7 +159,10 @@ def inc_count(dic, key):
         
 
 def usage():
-    return "\nevaluation_client.py <scene_file> <init_method> <start> <end>\n\n\t<scene_file>\tfile of test scenes\n\t<init_method>\tmethod to initialize object classes [random, occurrence, ground_truth_60, ground_truth]\n\t<start>\t\tfirst scene to evaluate\n\t<end>\t\tlast scene to evaluate (optional)\n"
+    return ("\nevaluation_client.py <scene_file> <init_method> <start> <end>\n"
+            "\n\t<scene_file>\tfile of test scenes\n"
+            "\t<init_method>\tmethod to initialize object classes [random, occurrence, ground_truth_N, ~library~]\n"
+            "\t<start>\t\tfirst scene to evaluate\n\t<end>\t\tlast scene to evaluate (optional)\n")
 
         
 if __name__ == "__main__":
@@ -250,9 +172,22 @@ if __name__ == "__main__":
         sys.exit(0)
 
     init_method = sys.argv[2]
-    if not init_method in ['random', 'occurrence', 'ground_truth', 'ground_truth_60', 'ground_truth_70','ground_truth_80','ground_truth_90']:
-        print usage()
-        sys.exit(0)
+    #if not init_method in ['random', 'occurrence', 'ground_truth', 'ground_truth_60', 'ground_truth_70','ground_truth_80','ground_truth_90']:
+        #print usage()
+        #sys.exit(0)
+        
+    perception_distribution = None
+    for i in range(1, 101):
+        if init_method == "ground_truth_{}".format(i):
+            perception_distribution =  p = perception.PerceptionProb.create_simple(OBJ_TYPES, i/100.0)
+            break
+    else:
+        if init_method == "random":
+            perception_distribution =  p = perception.PerceptionProb.create_simple(OBJ_TYPES, 1.0/len(OBJ_TYPES))
+        else:
+            perception_distribution =  p = perception.PerceptionProb.create_from_library(init_method)
+            
+        
         
     with open(sys.argv[1]) as scn_file:
 
@@ -268,7 +203,7 @@ if __name__ == "__main__":
         sum_p = 0.0
         
         for i in range(start,end + 1):
-            sum_p += evaluate_scene(i, scenes[i], init_method)
+            sum_p += evaluate_scene(i, scenes[i], perception_distribution)
 
         avg_p = sum_p / num_of_scenes
             
---
+++ strands_qsr_evalution/src/perception.py
@@ -0,0 +1,139 @@
+import random
+
+class PerceptionProb(object):
+    def __init__(self, object_types, values_matrix=None):
+        """
+        :Parameters:
+            values_matrix : string
+                a matrix of perception values, p(Z |  X). see bellow for examples.
+            object_types : list
+                list of object types that the value matrix represents. Column and row
+                order must match and match the order of this list.
+        """
+        print "Creating perception object"
+        print object_types
+        print values_matrix
+        print "*" * 10
+        self.object_types = object_types
+        self.prob_perceive_given_state = dict([(o,  dict([(o,  1)
+                                                          for o in object_types]))
+                                               for o in object_types])
+        
+        #TODO: check values matrix is good size etc
+        if values_matrix is not None:
+            perceived =  values_matrix.strip().split("\n")
+            for perceive, i in zip(object_types, perceived):
+                vals = i.split("\t")
+                for state, v in zip(object_types, vals):
+                    self.prob_perceive_given_state[perceive][state] = float(v)
+                    
+    def prob(self, perception, actual):
+        """
+		Return p(Z=val | X=val)
+        
+        :Parameters:
+            perception : string
+                the perceived class
+            actual : string
+                the actual class
+        """
+        if perception not in self.object_types:
+            raise Exception("Unknown object type for perception in model")
+        if actual not in self.object_types:
+            raise Exception("Unknown object type for actual object in perception model")
+        return self.prob_perceive_given_state[perception][actual]
+
+    def create_perception(self, state):
+        """
+        Create a simulated perception for the given state.
+        
+        :Parameters:
+            state : dictionary
+                dictionary of object name to actual object type
+        
+        Return: dictionary, object name to list of confidence values ordered as in self.object_types
+        
+        """
+        percept = {}
+        for name, ob in state.items():
+            i = random.randint(1, 100) / 100.0
+            s = 0
+            for perc in self.object_types:
+                s += self.prob_perceive_given_state[perc][ob]
+                if s >= i - 0.1e-10 : # account for rounding errors
+                    break
+            else:
+                raise Exception("Overshot probs, matrix was likely incomplete. Columns "
+                                "need to sum to 1")
+            # labeling it as perc
+            # assume p(Z|X)=p(X|Z) - ie the matrix is symetric and use
+            # as the confidence values
+            # TODO: think this
+            percept[name] = [self.prob(obj, perc) for obj in self.object_types]
+        return percept
+    
+    @classmethod
+    def create_simple(cls, objects,  prob):
+        """
+        Create an uncorrelated perception model. That is the p(Z=cup | X=cup)=prob,
+        p(Z=NOTcup | X=cup)=(1-prob)/(no. objects - 1)
+        :Parameters:
+            objects : list
+                the objects to create a perception model for
+            prob : float
+                the probability that an object is labeled correctly
+        """
+        p =  cls(objects)
+        assert prob <= 1.0
+        for o in objects:
+            for o2 in objects:
+                if o == o2:
+                    p.prob_perceive_given_state[o][o2] = prob
+                else:
+                    p.prob_perceive_given_state[o][o2] = (1.0 - prob) / (len(objects) - 1)
+        isinstance(p, PerceptionProb)
+        return p
+        
+    @classmethod
+    def create_from_library(cls, entry):
+        """
+        Create a perception model from the matrix already present in this file,
+        with variable name "entry"
+        
+        :Parameters:
+            entry : string
+            	the matrix name to use
+        """
+        # TODO: more complete name checking
+        valid_names = [i for i, v in globals().items()
+                       if type(v) == list and len(v) == 2 and type(v) == list]
+        if not entry in valid_names:
+            raise Exception("Trying to create unknown perception model")
+        return cls(globals()[entry][0], globals()[entry][1])
+        
+
+
+# Below are some perception matrices, see perception.gnumeric
+simple_42 = [
+    ["Monitor", "Cup", "Keyboard", "Pencil", "Telephone", "PC", "Mouse", "Lamp",
+     "Calculator", "Headphone", "Laptop", "MobilePhone", "Glass", "Stapler", "Keys", "Book", "Bottle"], 
+"""
+0.65	0.01	0.05	0.01	0.01	0.1	0.01	0.01	0.01	0.03	0.05	0.01	0.01	0.01	0.01	0.01	0.01
+0.01	0.38	0.01	0.01	0.03	0.01	0.06	0.02	0.02	0.02	0.01	0.02	0.2	0.02	0.03	0.01	0.14
+0.05	0.01	0.398	0.01	0.02	0.04	0.012	0	0.04	0.04	0.2	0.02	0.01	0.03	0.01	0.1	0.01
+0.01	0.01	0.01	0.64	0.01	0.01	0.01	0	0.03	0.01	0.01	0.1	0.01	0.1	0.01	0.02	0.01
+0.01	0.03	0.02	0.01	0.37	0.05	0.01	0.2	0.05	0.05	0.04	0.04	0.01	0.01	0.01	0.08	0.01
+0.1	0.01	0.04	0.01	0.05	0.7	0.01	0.01	0	0	0.01	0.01	0.01	0.01	0.01	0.01	0.01
+0.01	0.06	0.012	0.01	0.01	0.01	0.628	0.02	0.03	0.03	0.01	0.04	0.03	0.03	0.03	0.02	0.02
+0.01	0.02	0	0	0.2	0.01	0.02	0.67	0.02	0.01	0.01	0	0.01	0	0.01	0	0.01
+0.01	0.02	0.04	0.03	0.05	0	0.03	0.02	0.7	0.01	0.005	0.05	0.01	0.01	0.004	0.01	0.001
+0.03	0.02	0.04	0.01	0.05	0	0.03	0.01	0.01	0.781	0.001	0.001	0.005	0.005	0.005	0.001	0.001
+0.05	0.01	0.2	0.01	0.04	0.01	0.01	0.01	0.005	0.001	0.5	0.05	0.001	0.001	0.001	0.1	0.001
+0.01	0.02	0.02	0.1	0.04	0.01	0.04	0	0.05	0.001	0.05	0.609	0.01	0.01	0.01	0.01	0.01
+0.01	0.2	0.01	0.01	0.01	0.01	0.03	0.01	0.01	0.005	0.001	0.01	0.654	0	0.01	0.01	0.01
+0.01	0.02	0.03	0.1	0.01	0.01	0.03	0	0.01	0.005	0.001	0.01	0	0.734	0.01	0.01	0.01
+0.01	0.03	0.01	0.01	0.01	0.01	0.03	0.01	0.004	0.005	0.001	0.01	0.01	0.01	0.8	0.02	0.02
+0.01	0.01	0.1	0.02	0.08	0.01	0.02	0	0.01	0.001	0.1	0.01	0.01	0.01	0.02	0.579	0.01
+0.01	0.14	0.01	0.01	0.01	0.01	0.02	0.01	0.001	0.001	0.001	0.01	0.01	0.01	0.02	0.01	0.717
+"""
+]
