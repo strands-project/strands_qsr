606764e81858c57672bcd03ea4321c384991b8b6
cburbridge <cburbridge@gmail.com>
2014-06-28 18:43:41 +0200

    learned qualitators based on clustering (not working)

--- strands_qsr_learned_qualitators/scripts/create_qualitators.py
+++ strands_qsr_learned_qualitators/scripts/create_qualitators.py
@@ -21,6 +21,11 @@ from matplotlib.patches import Ellipse
 from scipy.cluster.hierarchy import linkage
 from scipy.cluster.vq import vq, kmeans
 
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+from sklearn.datasets.samples_generator import make_blobs
+#from sklearn.preprocessing import StandardScaler
+
 if __name__ == '__main__':
     ''' Main Program '''
     
@@ -59,7 +64,7 @@ if __name__ == '__main__':
         scenes = json.load(f)
     geo_states = [gs.GeometricState.from_scene_data(s) for s in scenes]
 
-    relatives = []
+    relatives = {} # []
 
     great_plot = {} #turned out not to be so great :-(
     for geo in geo_states:
@@ -78,7 +83,9 @@ if __name__ == '__main__':
                                                geo._objects[ob2].position)]
                 s1 = geo._objects[ob1].bbox.get_size()
                 s2 = geo._objects[ob2].bbox.get_size()
-                relatives.append( delta  + list(s1) + list(s2) )
+                if not relatives.has_key(pair):
+                    relatives[pair] = []
+                relatives[pair].append( delta  + list(s1) + list(s2) )
                 if not great_plot.has_key(pair):
                     great_plot[pair] = []
                 great_plot[pair].append(delta + list(s1) + list(s2) )
@@ -87,23 +94,54 @@ if __name__ == '__main__':
     print "Number object pair types:", len(great_plot.keys())
     print "Clustering samples..."
     
-    Z = kmeans(np.array(relatives), 20)
-    print "Kmeans clustering done, creating Gaussians"
-    clusters = vq(np.array(relatives), Z[0])
-    print clusters
-    print ".."
+    #Z = kmeans(np.array(relatives), 20)
+    #print "Kmeans clustering done, creating Gaussians"
+    #clusters = vq(np.array(relatives), Z[0])
+    #print clusters
+    #print ".."
+    
     qs =  qualitators.Qualitators()
-    for i, cluster in enumerate(Z[0]):
-        s = []
-        for pt, sample in zip(clusters[0], relatives):
-            #print pt
-            if pt == i:
-                #print pt, "==", i
-                s.append(sample)
-        print np.array(s)
-        g = ds.GaussKernel(np.array(s), 1)
-        q =  qualitators.SimpleGaussianQualitator("rel%d"%i, 2, g)
-        qs.add_qualitator(q)
+    for pair_relatives in relatives.values():
+        ##############################################################################
+        # Generate sample data
+        #centers = [[1, 1], [-1, -1], [1, -1]]
+        #X, labels_true = make_blobs(n_samples=750, centers=centers, cluster_std=0.4,
+                                    #random_state=0)
+        
+        #X = StandardScaler().fit_transform(np.array(relatives))
+        X = np.array(pair_relatives)
+        
+        ##############################################################################
+        # Compute DBSCAN
+        db = DBSCAN(eps=0.5, min_samples=10).fit(X)
+        labels = db.labels_
+        
+        # Number of clusters in labels, ignoring noise if present.
+        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+        sets = [[]] * n_clusters_
+        print "Cluster count=", n_clusters_
+        print labels
+        for l, rel in zip(labels, pair_relatives):
+            if l != -1:
+                sets[int(l)].append(rel)
+        for i, s in enumerate(sets):
+            g = ds.GaussKernel(np.array(s), 1)
+            q =  qualitators.SimpleGaussianQualitator("rel%d"%i, 2, g, std_threshold=0.3)
+            qs.add_qualitator(q)
+            
+        
+    #qs =  qualitators.Qualitators()
+    #for i, cluster in enumerate(Z[0]):
+        #s = []
+        #for pt, sample in zip(clusters[0], relatives):
+            ##print pt
+            #if pt == i:
+                ##print pt, "==", i
+                #s.append(sample)
+        #print np.array(s)
+        #g = ds.GaussKernel(np.array(s), 1)
+        #q =  qualitators.SimpleGaussianQualitator("rel%d"%i, 2, g)
+        #qs.add_qualitator(q)
         
     ## TODO: tidy this as it is hacked from some old code, 
     #dens = ds.DensityEstimator.createFromData(np.array(relatives), 0.8)
@@ -119,94 +157,94 @@ if __name__ == '__main__':
     ########################################################
     ## Plot the clusters
     ########################################################
-    #fig, ax = plt.subplots()
+    fig, ax = plt.subplots()
 
-    #def plot_cov_ellipse(cov, pos, nstd=2, ax=None, **kwargs):
-        #"""
-        #Plots an `nstd` sigma error ellipse based on the specified covariance
-        #matrix (`cov`). Additional keyword arguments are passed on to the 
-        #ellipse patch artist.
+    def plot_cov_ellipse(cov, pos, nstd=2, ax=None, **kwargs):
+        """
+        Plots an `nstd` sigma error ellipse based on the specified covariance
+        matrix (`cov`). Additional keyword arguments are passed on to the 
+        ellipse patch artist.
     
-        #Parameters
-        #----------
-            #cov : The 2x2 covariance matrix to base the ellipse on
-            #pos : The location of the center of the ellipse. Expects a 2-element
-                #sequence of [x0, y0].
-            #nstd : The radius of the ellipse in numbers of standard deviations.
-                #Defaults to 2 standard deviations.
-            #ax : The axis that the ellipse will be plotted on. Defaults to the 
-                #current axis.
-            #Additional keyword arguments are pass on to the ellipse patch.
+        Parameters
+        ----------
+            cov : The 2x2 covariance matrix to base the ellipse on
+            pos : The location of the center of the ellipse. Expects a 2-element
+                sequence of [x0, y0].
+            nstd : The radius of the ellipse in numbers of standard deviations.
+                Defaults to 2 standard deviations.
+            ax : The axis that the ellipse will be plotted on. Defaults to the 
+                current axis.
+            Additional keyword arguments are pass on to the ellipse patch.
     
-        #Returns
-        #-------
-            #A matplotlib ellipse artist
-        #"""
-        #def eigsorted(cov):
-            #vals, vecs = np.linalg.eigh(cov)
-            #order = vals.argsort()[::-1]
-            #return vals[order], vecs[:,order]
+        Returns
+        -------
+            A matplotlib ellipse artist
+        """
+        def eigsorted(cov):
+            vals, vecs = np.linalg.eigh(cov)
+            order = vals.argsort()[::-1]
+            return vals[order], vecs[:,order]
     
-        #if ax is None:
-            #ax = plt.gca()
+        if ax is None:
+            ax = plt.gca()
     
-        #vals, vecs = eigsorted(cov)
-        #theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+        vals, vecs = eigsorted(cov)
+        theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
     
-        ## Width and height are "full" widths, not radius
-        #width, height = 2 * nstd * np.sqrt(vals)
-        #ellip = Ellipse(xy=pos, width=width, height=height, angle=theta, **kwargs)
+        # Width and height are "full" widths, not radius
+        width, height = 2 * nstd * np.sqrt(vals)
+        ellip = Ellipse(xy=pos, width=width, height=height, angle=theta, **kwargs)
     
-        #ax.add_artist(ellip)
-        #return ellip
-    
-    ##for q in qs:
-        ##cov =  q._gaussian.cov[0:2, 0:2]
-        ##plot_cov_ellipse(cov, q._gaussian.mu[0:2], nstd=0.5, alpha=0.5)
-
-    #consider = []
-    #for rel, data in great_plot.items():
-        #if len(data) > 0:
-            #consider.append(rel)
-        
-    
-    #colormap = plt.cm.gist_ncar
-    #colours = [colormap(i) for i in np.linspace(0, 0.9, len(consider))]
-##    plt.rc('axes', color_cycle=['r', 'g', 'b', 'y'])
-    #isinstance(ax, plt.Axes)
-    #rel = np.array(relatives)
-    #ax.set_color_cycle(colours)
-    #lbls = []
-    #for rel in consider:
-        #data = great_plot[rel]
-        ##, data in great_plot.items():
-        #d = np.array(data)
-        #ax.plot(d[:, 0], d[:, 1], '.',  alpha=.5, markersize=5)
-        ##for i in range(0, d.shape[0]):
-            ##for qual in qs:
-                ##if qual(d[i, :]):
-                    ##x, y = zip(*[(d[i, 0], d[i, 1]),
-                                 ##(qual._gaussian.mu[0],  qual._gaussian.mu[1])])
-                    ##line, = ax.plot(x, y, 'go-', markersize=0)
-        ##lbls.append(rel)
-        
-    ##plt.legend(consider, ncol=4, loc='upper center', 
-           ##bbox_to_anchor=[0.5, 1.1], 
-           ##columnspacing=1.0, labelspacing=0.0,
-           ##handletextpad=0.0, handlelength=1.5,
-           ##fancybox=True, shadow=True)
+        ax.add_artist(ellip)
+        return ellip
     
     #for q in qs:
         #cov =  q._gaussian.cov[0:2, 0:2]
-        #plot_cov_ellipse(cov, q._gaussian.mu[0:2], nstd=0.4, alpha=0.5)
+        #plot_cov_ellipse(cov, q._gaussian.mu[0:2], nstd=0.5, alpha=0.5)
+
+    consider = ["Mouse-Keyboard"]
+    for rel, data in great_plot.items():
+        if len(data) > 0:
+            consider.append(rel)
+        
     
-    #plt.rc('text', usetex=True)
-    #fontsize = 20
-    #plt.xlabel(r'$X$',  fontsize=20)
-    #plt.ylabel(r'$Y$',  fontsize=20)
-    #plt.title(r'A test using $x$',  fontsize=20)
+    colormap = plt.cm.gist_ncar
+    colours = [colormap(i) for i in np.linspace(0, 0.9, len(consider))]
+#    plt.rc('axes', color_cycle=['r', 'g', 'b', 'y'])
+    isinstance(ax, plt.Axes)
+    rel = np.array(relatives)
+    ax.set_color_cycle(colours)
+    lbls = []
+    for rel in consider:
+        data = great_plot[rel]
+        #, data in great_plot.items():
+        d = np.array(data)
+        ax.plot(d[:, 0], d[:, 1], '.',  alpha=.5, markersize=5)
+        #for i in range(0, d.shape[0]):
+            #for qual in qs:
+                #if qual(d[i, :]):
+                    #x, y = zip(*[(d[i, 0], d[i, 1]),
+                                 #(qual._gaussian.mu[0],  qual._gaussian.mu[1])])
+                    #line, = ax.plot(x, y, 'go-', markersize=0)
+        #lbls.append(rel)
+        
+    #plt.legend(consider, ncol=4, loc='upper center', 
+           #bbox_to_anchor=[0.5, 1.1], 
+           #columnspacing=1.0, labelspacing=0.0,
+           #handletextpad=0.0, handlelength=1.5,
+           #fancybox=True, shadow=True)
     
-    #plt.show()
+    for q in qs:
+        cov =  q._gaussian.cov[0:2, 0:2]
+        plot_cov_ellipse(cov, q._gaussian.mu[0:2], nstd=0.4, alpha=0.5)
+    
+    plt.rc('text', usetex=True)
+    fontsize = 20
+    plt.xlabel(r'$X$',  fontsize=20)
+    plt.ylabel(r'$Y$',  fontsize=20)
+    plt.title(r'A test using $x$',  fontsize=20)
+    
+    plt.show()
 
     
     ######################################################
